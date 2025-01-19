c13fa3cdedb36b78d5897eca90215ad67c92793d
prabowo-rifqi <praboworifqi@student.gunadarma.ac.id>
2025-01-19 16:15:55 +0700

    Update Deploy Stage Jenkinsfile

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to !</h1>
+          <h1 className="App-title">Welcome to React Rifqi Aditya!</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload. KONTOL
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
